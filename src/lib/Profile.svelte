4faa29e5099be770fd04877aa2a81a10c204a8f7
Halftime Harry <ddinsmore8@gmail.com>
2022-06-02 12:51:08 +0000

    Using the profile as a guide create a bank table

--- src/lib/Profile.svelte
+++ src/lib/Profile.svelte
@@ -51,7 +51,7 @@
 
 			let { data, error, status } = await supabase
 				.from("profiles")
-				.select(`current_balance, amount_at_risk, available_ballance, twitter_link`)
+				.select(`current_balance, amount_at_risk, available_ballance`)
 				.eq("id", user.id)
 				.single();
 
@@ -86,43 +86,43 @@
 				<div class="form-group">
 					<label for="email">Email</label>
 					<input
-						id="email" class="form-control" type="text" value={$user.email} disabled
+						id="email" class="form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" value={$user.email} disabled
 					/>
 				</div>
 				<div class="form-group">
 					<label for="current_balance">Current Balance</label>
 					<input
-						id="current_balance" class="form-control" type="text"
+						id="current_balance" class="form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text"
 						bind:value={current_balance}
 					/>
 				</div>
 				<div class="form-group">
 					<label for="amount_at_risk">Amount at risk</label>
 					<input
-						id="amount_at_risk" type="text" class="form-control"
+						id="amount_at_risk" type="text" class="form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						bind:value={amount_at_risk}
 					/>
 				</div>
 				<div class="form-group">
 					<label for="available_ballance">Available Ballance</label>
-					<textarea id="available_ballance" class="form-control" bind:value={available_ballance} />
+					<textarea id="available_ballance" class="form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" bind:value={available_ballance} />
 				</div>
 				<div class="form-group">
 					<label for="twiiter_link">Twitter link</label>
 					<input
-						id="twiiter_link" type="text" class="form-control"
+						id="twiiter_link" type="text" class="form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						bind:value={twitter_link}
 					/>
 				</div>
 
 				<div>
-					<input type="submit" class="btn btn-primary" value={loading ? "Loading ..." : "Update"}
+					<input type="submit" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900" value={loading ? "Loading ..." : "Update"}
 						disabled={loading}
 					/>
 				</div>
 			</form>
 			{#if message.success != null}
-				<div class="alert {message.success ? 'alert-success' : 'alert-danger'}" role="alert">
+				<div class="alert {message.success ? 'p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800' : 'p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800'}" role="alert">
 					{message.display}
 				</div>
 			{/if}
